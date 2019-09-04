38714020cdd4d1670695e103a71e73c6447fbde1
jaksani <43771443+jaksani@users.noreply.github.com>
2019-09-04 10:30:38 +0530

    Added SQL Scripts

---
+++ Database/WareHouseManagement_purchase_details.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: WareHouseManagement
+-- ------------------------------------------------------
+-- Server version	5.7.27-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `purchase_details`
+--
+
+DROP TABLE IF EXISTS `purchase_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase_details` (
+  `transaction_id` int(11) NOT NULL,
+  `customer_code` int(11) NOT NULL,
+  `date_of_purchase` date DEFAULT NULL,
+  `item_code` int(11) NOT NULL,
+  `purchase_amount` double NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_details`
+--
+
+LOCK TABLES `purchase_details` WRITE;
+/*!40000 ALTER TABLE `purchase_details` DISABLE KEYS */;
+INSERT INTO `purchase_details` VALUES (2,1,'2019-08-29',1,1287,10),(3,1,'2019-08-29',1,1287,10),(4,1,'2019-08-29',1,1287,10),(7,1,'2019-08-30',1,12987,100),(8,1,'2019-08-30',1,12987,100),(9,1,'2019-08-30',1,1287,10);
+/*!40000 ALTER TABLE `purchase_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-09-03 13:33:15
